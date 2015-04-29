c3adce7b39f6d9000203dbca7dd1b38a7e483d4c
Soutar <john@soutar.me>
2015-04-29 15:09:44 +0100

    Convert to ES6 with a Gulp/Babel build step

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp       = require('gulp');
+var babel      = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify     = require('gulp-uglify');
+var gulpif     = require('gulp-if');
+
+var debug = false;
+
+gulp.task('es6', function () {
+    gulp.src('src/bem.es6.js')
+        .pipe(gulpif(debug, sourcemaps.init()))
+            .pipe(babel())
+            .pipe(uglify())
+        .pipe(gulpif(debug, sourcemaps.write()))
+        .pipe(gulp.dest('dist/bem.js'))
+});
+
+gulp.task('dev', function () {
+    debug = true;
+    gulp.watch('src/bem.js', ['es6']);
+});
