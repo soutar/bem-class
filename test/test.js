713011feba1211c2f0d69822a99c1ce8f49db477
BuildTools <john@soutar.me>
2015-05-28 22:29:47 +0100

    Add Mocha tests

---
+++ test/test.js
@@ -0,0 +1,192 @@
+import assert from 'assert';
+import { BEM_Modifiable, BEM_Block, BEM_Element } from '../src/bem';
+
+describe('BEM_Modifiable', () => {
+  describe('constructor()', () => {
+    it('should take a name and set it on the instance', () => {
+      var modifiable = new BEM_Modifiable('modifiable');
+
+      assert.equal('modifiable', modifiable.name);
+    });
+
+    it('should throw an error if a non-string name is provided', () => {
+      assert.throws(() => {
+        var modifiable = new BEM_Modifiable();
+      }, Error);
+    });
+  });
+
+  describe('modifier()', () => {
+    it('should apply truthy modifiers', () => {
+      let modifiable = new BEM_Modifiable('modifiable').modifier({
+        'enabled': true
+      });
+
+      assert.equal(true, (modifiable.modifiers.indexOf('enabled') > -1));
+    });
+
+    it('should ignore falsey modifiers', () => {
+      let modifiable = new BEM_Modifiable('modifiable').modifier({
+        'disabled': false
+      });
+
+      assert.equal(-1, modifiable.modifiers.indexOf('disabled'));
+    });
+  });
+
+});
+
+describe('BEM_Block', () => {
+  describe('constructor()', () => {
+    it('should take a name and set it on the instance', () => {
+      var block = new BEM_Block('block');
+
+      assert.equal('block', block.name);
+    });
+
+    it('should throw an error if a non-string name is provided', () => {
+      assert.throws(() => {
+        var block = new BEM_Block();
+      }, Error);
+    });
+  });
+
+  describe('element()', () => {
+    it('should return a new BEM_Element', () => {
+      let element = new BEM_Block('block').element('element');
+
+      assert.equal(true, (element instanceof BEM_Element));
+    });
+  });
+
+  describe('toString()', () => {
+    it('should return only the block class when no modifiers are applied', () => {
+      let block = new BEM_Block('block-name');
+
+      assert.equal('block-name', block.toString());
+    });
+
+    it('should return a space seperated list of class names when modifiers are applied', () => {
+      let block = new BEM_Block('block-name').modifier({
+        'modifier1': true,
+        'modifer2': true
+      });
+
+      assert.equal(true, (block.toString().split(' ').length > 1));
+    });
+
+    it('should include truthy modifiers', () => {
+      let block = new BEM_Block('block-name').modifier({
+        'modifier1': true,
+        'modifier2': true
+      });
+
+      assert.equal(true, (block.toString().indexOf('block-name--modifier1') > -1));
+      assert.equal(true, (block.toString().indexOf('block-name--modifier2') > -1));
+    });
+
+    it('should exclude falsey modifiers', () => {
+      let block = new BEM_Block('block-name').modifier({
+        'modifier1': false,
+        'modifier2': false
+      });
+
+      assert.equal(true, (block.toString().indexOf('block-name--modifier1') === -1));
+      assert.equal(true, (block.toString().indexOf('block-name--modifier2') === -1));
+    });
+
+    it('should contain n+1 classes in a space separated list, where n is the amount of truthy modifiers', () => {
+      let modifiers = {
+        'modifier1': false,
+        'modifier2': true,
+        'modifier3': true,
+        'modifier4': false
+      };
+      let block = new BEM_Block('block-name').modifier(modifiers);
+
+      assert.equal(
+        Object.keys(modifiers).filter(modifierName => !!modifiers[modifierName]).length + 1,
+        block.toString().split(' ').length);
+    });
+  });
+});
+
+describe('BEM_Element', () => {
+  describe('constructor()', () => {
+    it('should take a name and set it on the instance', () => {
+      var block = new BEM_Block('block');
+      var element = new BEM_Element(block, 'element');
+
+      assert.equal('element', element.name);
+    });
+
+    it('should throw an error if a non-string name is provided', () => {
+      assert.throws(() => {
+        var block = new BEM_Block('block');
+        var element = new BEM_Element(block, null);
+      }, Error);
+    });
+
+    it('should throw an error if a BEM_Block is not provided', () => {
+      assert.throws(() => {
+        var element = new BEM_Element();
+      }, Error);
+    });
+  });
+
+  describe('toString()', () => {
+    it('should return only the element class when no modifiers are applied', () => {
+      let block = new BEM_Block('block-name');
+      let element = new BEM_Element(block, 'element-name');
+
+      assert.equal('block-name__element-name', element.toString());
+    });
+
+    it('should return a space seperated list of class names when modifiers are applied', () => {
+      let block = new BEM_Block('block-name');
+      let element = new BEM_Element(block, 'element-name').modifier({
+        'modifier1': true,
+        'modifer2': true
+      });
+
+      assert.equal(true, (element.toString().split(' ').length > 1));
+    });
+
+    it('should include truthy modifiers', () => {
+      let block = new BEM_Block('block-name');
+      let element = new BEM_Element(block, 'element-name').modifier({
+        'modifier1': true,
+        'modifier2': true
+      });
+
+      assert.equal(true, (element.toString().indexOf('block-name__element-name--modifier1') > -1));
+      assert.equal(true, (element.toString().indexOf('block-name__element-name--modifier2') > -1));
+    });
+
+    it('should exclude falsey modifiers', () => {
+      let block = new BEM_Block('block-name');
+      let element = new BEM_Element(block, 'element-name').modifier({
+        'modifier1': false,
+        'modifier2': false
+      });
+
+      assert.equal(true, (element.toString().indexOf('block-name--modifier1') === -1));
+      assert.equal(true, (element.toString().indexOf('block-name--modifier2') === -1));
+    });
+
+    it('should contain n+1 classes in a space separated list, where n is the amount of truthy modifiers', () => {
+      let modifiers = {
+        'modifier1': false,
+        'modifier2': true,
+        'modifier3': true,
+        'modifier4': false
+      };
+      let block = new BEM_Block('block-name');
+      let element = new BEM_Element(block, 'element-name').modifier(modifiers);
+      
+      assert.equal(
+        Object.keys(modifiers).filter(modifierName => !!modifiers[modifierName]).length + 1,
+        element.toString().split(' ').length);
+    });
+  });
+});
